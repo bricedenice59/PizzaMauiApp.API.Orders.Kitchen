efdc5414ef57dceb7d07481a14abbc0d067f7cbb
Brice Grenard <brice.grenard@gmail.com>
2024-02-27 20:03:16 +0100

    in kitchen events, UserID is a required string. Updates with latest local RabbitMq.Messages library version.

--- Contracts/KitchenCanceledOrderEvent.cs
+++ Contracts/KitchenCanceledOrderEvent.cs
@@ -6,10 +6,10 @@ namespace PizzaMaui.API.Orders.Kitchen.Contracts
     {
         public Guid CorrelationId { get; init; }
         public Guid OrderId { get; set; }
-        public Guid UserId { get; set; }
+        public required string UserId { get; set; }
         
         public DateTime CreatedAt { get; set; }
-        
-        public List<IOrderItem> Items { get; set; }
+
+        public List<IOrderItem> Items { get; set; } = new();
     }
 }
--- StateMachines/KitchenCookerState.cs
+++ StateMachines/KitchenCookerState.cs
@@ -9,7 +9,7 @@ namespace PizzaMaui.API.Orders.Kitchen.StateMachines
         public int CurrentState { get; set; }
         public Guid CorrelationId { get; set; }
         public Guid OrderId { get; set; }
-        public Guid UserId { get; set; }
+        public required string UserId { get; set; }
         public DateTime CreatedAt { get; set; }
         public List<IOrderItem> Items { get; set; } = new();
     }
